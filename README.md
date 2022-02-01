ae82bf240a942698121aba18349179c9cb4f8a1d
Emanuele Guicciardi <eguicciardi@users.noreply.github.com>
2022-02-01 17:43:04 +0100

    Initial commit
    February 2022 version

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # mac-config
-Quick and dirty setup scripts and configurations
+
+This is the script I usually launch setting up a new machine. Lately I choose to exclude from backups executables and applications so bootstrapping a new machine quickly is essential for having as little downtime as possible.
+
+The script is heavily based on the [Homebrew](https://brew.sh) package manager and at the end it sets some very opinionated defaults in MacOS.
+
+## Disclaimer
+
+The script is provided as-is, please read it carefully before running it.
---
+++ install.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+
+# Check for Homebrew
+if test ! $(which brew); then
+    echo "Installing homebrew..."
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+brew update
+
+# Install GNU core utilities
+brew tap homebrew/dupes
+brew install coreutils
+brew install gnu-sed --with-default-names
+brew install gnu-tar --with-default-names
+brew install gnu-indent --with-default-names
+brew install gnu-which --with-default-names
+brew install gnu-grep --with-default-names
+
+
+PACKAGES=(
+    mongodb-community
+    mongodb-database-tools
+    mongosh
+    mysql@5.7
+    php
+    sqlite
+    starship
+    wget
+    youtube-dl
+)
+
+echo "Installing packages..."
+brew install ${PACKAGES[@]}
+brew cleanup
+
+echo "Installing cask..."
+brew install caskroom/cask/brew-cask
+
+CASKS=(
+    adobe-creative-cloud
+    android-studio
+    db-browser-for-sqlite
+    docker
+    dropbox
+    font-hack-nerd-font
+    google-chrome
+    imageoptim
+    iterm2
+    microsoft-teams
+    mongodb-compass
+    mysqlworkbench
+    notion
+    pastebot
+    postman
+    sequel-pro
+    slack
+    spotify
+    telegram
+    transmit
+    visual-studio-code
+    vlc
+    webex
+    whatsapp
+    xcodes
+    yed
+    zoom
+)
+
+echo "Installing cask application, pay attention for password requests"
+brew cask install ${CASKS[@]}
+
+# Require password as soon as screensaver or sleep mode starts
+defaults write com.apple.screensaver askForPassword -int 1
+defaults write com.apple.screensaver askForPasswordDelay -int 0
+
+# Disable "natural" scroll
+defaults write NSGlobalDomain com.apple.swipescrolldirection -bool false
+
+echo "I think we are done, get back to work!"
